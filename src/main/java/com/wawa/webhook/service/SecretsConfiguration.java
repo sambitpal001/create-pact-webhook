aca1c32688ea1dd90b7db070cd3a67f5da633528
SambitKumarPal <sambit.pal@wawa.com>
2020-10-06 07:28:51 -0400

    secreates commented
    secreates commented

--- src/main/java/com/wawa/webhook/service/SecretsConfiguration.java
+++ src/main/java/com/wawa/webhook/service/SecretsConfiguration.java
@@ -4,10 +4,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
-@Configuration
+/*@Configuration
 @PropertySources(
     @PropertySource(value = "file:${SECRETSVOL}/secrets.properties")
-)
+)*/
 public class SecretsConfiguration {
 
 }
